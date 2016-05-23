2f42c0b57883620dec2b5ea64ee605c4a4b97cea
Pavel Pichrt <pavel.pichrt@socialbakers.com>
2016-05-23 11:00:19 +0100

    v0.0.2

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 'use strict'
 
-const createError = require('create-error')
+const Errors = require('./lib/errors')
+const utils = require('./lib/utils')
 
-module.exports = {
-  FileTooLargeError: createError('FileTooLargeError'),
-  TypeError: createError('TypeError'),
-  ReadError: createError('ReadError'),
-  InvalidIdError: createError('InvalidIdError'),
-  TokenNotFoundError: createError('TokenNotFoundError'),
-  TokenTypeError: createError('TokenTypeError'),
-  EmailAlreadyRegisteredError: createError('EmailAlreadyRegisteredError'),
-  NotFoundError: createError('NotFoundError'),
-  NotUpdatedError: createError('NotUpdatedError'),
-  NotRemovedError: createError('NotRemovedError'),
-  UserActivatedError: createError('UserActivatedError'),
-  UserDeActivatedError: createError('UserDeActivatedError'),
-  UserPendingError: createError('UserPendingError'),
-  InvalidDataError: createError('InvalidDataError'),
-  RecordCreationError: createError('RecordCreationError'),
-  InvalidTokenError: createError('InvalidTokenError'),
-  TokenExpiredOrRevokedError: createError('ExpiredOrRevokedTokenError'),
-  NullOrUndefinedReferenceError: createError('NullOrUndefinedReferenceError'),
-  PasswordError: createError('PasswordError'),
-  ReactivationPeriodNotElapsedError: createError('ReactivationPeriodNotElapsedError'),
-  PermissionNotFoundError: createError('PermissionNotFoundError'),
-  InvalidEmail: createError('InvalidEmail'),
-  InvalidPasswordError: createError('InvalidPasswordError'),
-  InvalidLoginAttempt: createError('InvalidLoginAttempt'),
-  ActivationCodeGenerationError: createError('ActivationCodeGenerationError'),
-  ActivationCodeError: createError('ActivationCodeError'),
-  UnauthorizedError: createError('UnauthorizedError')
-}
+let exp = Errors
+exp.utils = utils
+
+module.exports = exp
