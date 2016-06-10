ea8b18220d9b63adb75fafa5d36c71a05014d154
Pavel Pichrt <pavel.pichrt@socialbakers.com>
2016-06-10 14:42:23 +0100

    start scripts

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ const chai = require('chai')
 const Errors = require('../index')
 const createError = require('create-error')
 const Boom = require('boom')
+const expect = require('chai').expect
 
 chai.should()
 
