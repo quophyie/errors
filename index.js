4d9b85104fe4732d8232951c9b8ca903c5439d33
quophyie <quophyie@yahoo.com>
2017-07-04 01:38:18 +0100

    Added express error middleware to index

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const Errors = require('./lib/errors')
 const utils = require('./lib/utils')
+const errorMiddleware = require('./lib/express-error-middleware')
 
 let exp = Errors
 exp.utils = utils
+exp.expressErrorMiddleware = errorMiddleware
 
 module.exports = exp
